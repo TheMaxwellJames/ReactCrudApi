9b799aff88e60cff583f7fca3698af3318d1a6bb
TheMaxwellJames <maxichanj@gmail.com>
2023-11-28 16:28:09 +0100

    first commit

---
+++ reactcrud/src/components/Loading.js
@@ -0,0 +1,19 @@
+
+
+
+function Loading()
+{
+    return (
+        <div className="container mt-3">
+
+       
+        <div className="spinner-border text-warning" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div> 
+
+<span>Loading... Please Wait</span>
+
+</div>
+    )
+}
+export default Loading;
